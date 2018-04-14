d3f784879bebab0282e5734adada7b6b8b274d51
wangfuguo <wangfuguo@zeyihuoyun.com>
2018-04-14 10:31:29 +0800

    Initial commit

---
+++ src/main/java/com/apache/App.java
@@ -0,0 +1,13 @@
+package com.apache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
